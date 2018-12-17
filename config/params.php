03e74b04775a42768aff59e08dd472ca25f4a5ad
Bruce Xie <xiediebruce@hotmail.com>
2018-12-17 21:31:00 +0800

    modify default config

--- config/params.php
+++ config/params.php
@@ -5,7 +5,7 @@ return [
 	'quickSearch' => ['aaa', 'bbb', 'ccc'],
 	'pageSize' => 10,
 	//two value: popup / inline
-	'valDisplayType' => 'inline',
+	'valDisplayType' => 'popup',
 	// only use this option where valDisplayType sets to 'popup'
 	// popup modal width(css), e.g.: 50%, 500px, 50em, 70rem or empty string
 	'modalWidth' => '50%',
