fdcb4bc37b8a013bc8f843d251800d40cd94a3f5
Bruce Xie <xiediebruce@hotmail.com>
2019-02-22 16:36:03 +0800

    update config/redis.php

--- config/redis.php
+++ config/redis.php
@@ -5,11 +5,13 @@
  * Date: 2018-12-13
  * Time: 16:57
  */
-
+	
 return [
-	'class' => 'Predis\Client',
-	'scheme' => 'tcp',
-	'host'   => '127.0.0.1',
-	'port'   => 6379,
-	'password' => '123',
+	'class' => 'mojifan\redis\Connection',
+	'servers' => [
+		'scheme' => 'tcp',
+		'host'   => '127.0.0.1',
+		'port'   => 6378,
+		'password' => '123',
+	]
 ];
